4b0e54278f83fa95963ee6dd7be7ee36f604721c
Wu-zpeng <56808242+Wu-zpeng@users.noreply.github.com>
2026-04-14 20:43:17 +0800

    添加google AdSense 代码

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -66,6 +66,14 @@ export default async function LocaleLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google AdSense */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1825672195873677"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-FYKSHQL9JZ"
